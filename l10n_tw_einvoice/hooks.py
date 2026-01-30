2af0468feb582b8392ceb739799062ece00fac15
jameslu34 <115187076+jameslu34@users.noreply.github.com>
2026-01-31 01:13:06 +0800

    Delete l10n_tw_einvoice directory

--- l10n_tw_einvoice/hooks.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, SUPERUSER_ID
-
-def post_init_hook(env):
-    report = env.ref('l10n_tw_einvoice.action_report_tw_invoice', raise_if_not_found=False)
-    paperformat = env.ref('l10n_tw_einvoice.paperformat_tw_invoice_57mm', raise_if_not_found=False)
-    if report and paperformat:
-        report.write({'paperformat_id': paperformat.id})
--- l10n_tw_einvoice/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_company
-from . import einvoice_track
-from . import einvoice_lottery
-from . import account_move
-from . import res_config_settings
